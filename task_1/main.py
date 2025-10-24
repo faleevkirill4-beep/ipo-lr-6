a1ea999bdcb0222eca25a297f962c1ad2e6f4732
Kirill <Faleevkirill4@gmail.com>
2025-10-24 10:23:50 +0300

    cc

---
+++ task_1/main.py
@@ -0,0 +1,20 @@
+#варант 1
+text_1 = [
+"sdsda",
+"sdda",
+"wwwww",
+"aaaa",
+"",
+"car"]
+
+count = 0
+
+for a in text_1:
+ if "a" in a:
+   count+=1
+
+
+
+print(count)
+
+
---
+++ task_2/main.py
@@ -0,0 +1,9 @@
+#вариант 1 
+import random
+
+height = random.randint(4,8)
+length = random.randint(4,8)
+a = [-3, -5, -2, -12, 0, 15, 4, 7, 2]
+
+b = [[height]]
+print(b)
