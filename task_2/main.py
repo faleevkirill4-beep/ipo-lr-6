1741c37a303b18df7a9190760849fc4ca161278e
Kirill <Faleevkirill4@gmail.com>
2025-10-24 10:37:11 +0300

    системник

--- task_2/main.py
+++ task_2/main.py
@@ -1,9 +1,20 @@
 #вариант 1 
 import random
 
+summ = 0
 height = random.randint(4,8)
 length = random.randint(4,8)
 a = [-3, -5, -2, -12, 0, 15, 4, 7, 2]
 
-b = [[height]]
-print(b)
+b =  [[random.choice(a) for j in range(height)] for i in range(length)]
+
+
+for char in b:
+    print(char)
+    for i in char:
+        if i % 2== 0:
+           summ+=i
+
+print(summ)
+
+
