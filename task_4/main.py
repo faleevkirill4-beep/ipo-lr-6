8e714a339b4256485115ad6675f520beea0c380c
Kirill <Faleevkirill4@gmail.com>
2025-11-20 12:29:35 +0300

    ыыыы

--- task_4/main.py
+++ task_4/main.py
@@ -1,4 +1,26 @@
 #Kirill
-n = input("введите искомую строку: ")
-with open("text.txt",encoding = 'UTF - 8',) as file:
+#ввод строки 
+n = input("введите искомую строку: ").strip().lower()
+#открытие файла из которого будем брать данные 
+with open("text.txt",'r',encoding = 'UTF - 8') as file:
+    lines = file.readlines()#реобразования строк в список 
+
+listik = []
+number = 0
+#нахождение строки в файле 
+for i,line in enumerate(lines,1):
+    if n in line.lower():
+        listik.append(line.strip())
+        number+=1
+
+print("список сток: " , listik)
+print("колличество строк: " , number)
+
+#исправить сортировку
+
+#сортировка 
+listik_sort =sorted(listik, key=lambda x: len(x[1]))
+
+print(listik_sort)
+
     
