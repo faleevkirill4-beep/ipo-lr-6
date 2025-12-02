2ec4018ab2c6d077f74fa6f9d5bc269c649fee3c
kirill <faleevkirill4@gmail.com>
2025-12-02 20:13:33 +0000

    оперсис

--- task_6/main.py
+++ task_6/main.py
@@ -12,20 +12,34 @@ unique_combinations = list({tuple(sorted(sublist)) for sublist in list_1})
 
 print(f"\nВсего уникальных комбинаций: {len(unique_combinations)}")
 print("уникальные комбинации: ")
-for i,a in enumerate(unique_combinations):
-    print(f"{i+1:2d}: {a}")
+for i,a in enumerate(unique_combinations,1):
+    print(f"{i:2d}: {a}")
 
 
-s = input("введите число: ")
+s = int(input("введите число: "))
 
-sum_1 = 0
-count_i = 0
+count = 0
+total_sum = 0
+groups = []
 
-for i in unique_combinations :
-    if(i < s):
-        count_i+=1
+for i in range(len(unique_combinations)):
+    for j in range(i+1,len(unique_combinations)):
+        total_sum = int(sum(unique_combinations[i]) + int(sum(unique_combinations[j])))
 
-print(coount_i)
+    if total_sum < s:
+        count += 1
+    groups.append((unique_combinations[i],unique_combinations[j],total_sum))
+
+print(f"\Колличество пар, чья сумма меньше {s}:{count}")
+
+if count > 0:
+    print("Пары и их суммы:")
+    for i, (group_1,group_2,sum_1) in enumerate(groups,1):
+        if sum_1 < s:
+            print(f"{i:2}: {group_1} + {group_2} = {sum_1}")
+else:
+    print("не найдено")
+
 
 
  
